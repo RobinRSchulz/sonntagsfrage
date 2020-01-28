0dae0d68d4bf7865d4a1fa189c997f7ce535e6ff
Robin Schulz <uyduk@student.kit.edu>
2020-01-28 15:48:53 +0100

    Initial commit

---
+++ getData.py
@@ -0,0 +1,80 @@
+import requests, json, os
+
+try:
+   from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup 
+
+def getTableFromWahlrechtSubPage(url:str):
+    request = requests.get(url)
+    parsed_html = BeautifulSoup(request.content)
+    table = parsed_html.body.find('table', attrs={'class':'wilko'})
+    table_head = table.find('thead')
+    table_body = table.find('tbody')
+    
+    parties_list = getPartiesFromTableHead(table_head)
+
+    parsed_table:map = {}
+    for table_row in table_body.find_all('tr'):
+        parsed_row:list = []
+        for table_cell in table_row.find_all('td'):
+            if len(table_cell.contents) != 0:
+                content = table_cell.contents[0]
+
+                if "%" in str(content):
+                    parsed_row.append(content)
+        try:
+            date_cell = table_row.find_all('td', attrs={'class':'s'})[0]
+            parsed_table[str(date_cell.contents[0])] = parsed_row
+        except IndexError:
+            None
+
+    return {'parties':parties_list, 'values':parsed_table}
+
+
+def getPartiesFromTableHead(table_head:BeautifulSoup):
+    parties_soup = table_head.find_all('th', attrs={'class':'part'})
+    parties:list = []
+    for party_elem in parties_soup:
+        party_string:str = ''
+        for contained_element in party_elem.contents:
+            if hasattr(contained_element, 'contents'):
+                party_string += str(contained_element.contents[0])
+            else:
+                party_string += str(contained_element)
+        parties.append(party_string)
+    return parties
+
+def main():
+    print("Executing main function:")
+    # request = requests.get('https://www.wahlrecht.de/umfragen/forsa.htm')
+    # print("######################################BEGIN########################################")
+    # for line in request.iter_lines():
+    #     print(line)
+    # print("######################################END########################################")
+
+    # parsed_html = BeautifulSoup(request.content)
+    # print(parsed_html.body.find('table', attrs={'class':'wilko'}).text)
+    data:map = {}
+    data['forsa'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/forsa.htm')
+    data['allensbach'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/allensbach.htm')
+    data['emnid'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/emnid.htm')
+    data['politbarometer'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/politbarometer.htm')
+    data['gms'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/gms.htm')
+    data['dimap'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/dimap.htm')
+    data['insa'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/insa.htm')
+    data['yougov'] = getTableFromWahlrechtSubPage('https://www.wahlrecht.de/umfragen/yougov.htm')
+    
+    dir:str = "C:\\Users\\Robin\\Google Drive\\Privat\\IONOS\\Sonntagsfrage-Website\\data\\"
+    try:
+        os.stat(dir)
+    except:
+        os.mkdir(dir) 
+    for datum in data.items():
+        fileName = dir + str(datum[0]) + '.json'
+        with open(fileName, 'w') as fp:
+            json.dump(datum[1], fp)
+        print("stored " + fileName + ".")
+
+if __name__== "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+import json
+
+
+#methods
+def getJsonDataFromFile():
+    insaFile = open("C:\\Users\\Robin\\Google Drive\\Privat\\IONOS\\Sonntagsfrage-Website\\data\\insa.json")
+    jsonData = json.load(insaFile)
+    return jsonData
+
+def getParties(valueMap:map):
+    return valueMap["parties"]
+
+def reverse(l:list):
+    l.reverse()
+    return l
+
+def getValuesSorted(valueMap:map, index:int):
+    s:str = "25.09.2012"
+    keysSorted = getKeysSorted(valueMap)
+    values:list = []
+    for key in keysSorted:
+        percentageValues:list = valueMap["values"][key]
+        if (len(percentageValues) > index):
+            values.append(percentageValues[index])
+        else:
+            values.append('')
+    return values
+
+def getKeysSorted(valueMap:map):
+    return sorted(valueMap["values"].keys(), key=lambda d: '.'.join(reverse(d.split('.'))))
+
+#variables
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return getJsonDataFromFile()
+    # return render_template('sonntagsfrage.htm')
+
+if __name__ == '__main__':
+    jsonData = getJsonDataFromFile()
+    print(getParties(jsonData))
+    print(getValuesSorted(jsonData, 3))
+    app.run(debug=True)
